a30e494f3c5beb069da327265be730869a62a15b
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2024-03-16 12:13:34 -0400

    Time: 2 ms (98.94%), Space: 42.8 MB (82.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,21 +1,47 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         String regex = "[^a-zA-Z0-9\\s]";
+//         int n = s.length();
+        
+//         String rs = s.replaceAll(regex, "").replaceAll(" ","").toLowerCase();
+//         System.out.println(rs);
+//         int i=0;
+//         int j=rs.length()-1;
+//         System.out.println(i + " "+j);
+//         while(i<=j){
+//             if(rs.charAt(i)!=rs.charAt(j)){
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+        
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isPalindrome(String s) {
-        String regex = "[^a-zA-Z0-9\\s]";
-        int n = s.length();
-        
-        String rs = s.replaceAll(regex, "").replaceAll(" ","").toLowerCase();
-        System.out.println(rs);
-        int i=0;
-        int j=rs.length()-1;
-        System.out.println(i + " "+j);
-        while(i<=j){
-            if(rs.charAt(i)!=rs.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
+        if (s.isEmpty()) {
+        	return true;
         }
-        
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+        	char currFirst = s.charAt(start);
+        	char currLast = s.charAt(last);
+        	if (!Character.isLetterOrDigit(currFirst )) {
+        		start++;
+        	} else if(!Character.isLetterOrDigit(currLast)) {
+        		last--;
+        	} else {
+        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+        			return false;
+        		}
+        		start++;
+        		last--;
+        	}
+        }
         return true;
     }
 }
