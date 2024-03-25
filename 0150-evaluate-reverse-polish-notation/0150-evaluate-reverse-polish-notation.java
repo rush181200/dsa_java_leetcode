38fd0df6f7d4074bb29f9b153b577e6b6af9d9bc
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2024-03-25 18:47:31 -0400

    Time: 6 ms (73.41%), Space: 44.8 MB (23.36%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,64 +1,89 @@
-class Solution {
-    public boolean isNotValidNumber(String s) {
-        try {
-            Double.parseDouble(s); // or Integer.parseInt(s) for integer check
-            return false; // If no exception is thrown, it's a valid number
-        } catch (NumberFormatException e) {
-            return true; // If an exception is thrown, it's not a valid number
-        }
-    }
-    public int evalRPN(String[] tokens) {
+// class Solution {
+//     public boolean isNotValidNumber(String s) {
+//         try {
+//             Double.parseDouble(s); // or Integer.parseInt(s) for integer check
+//             return false; // If no exception is thrown, it's a valid number
+//         } catch (NumberFormatException e) {
+//             return true; // If an exception is thrown, it's not a valid number
+//         }
+//     }
+//     public int evalRPN(String[] tokens) {
         
-        Stack<Integer> st = new Stack<>();
+//         Stack<Integer> st = new Stack<>();
        
         
-        for(int i=0;i<tokens.length;i++){
+//         for(int i=0;i<tokens.length;i++){
             
-            if(isNotValidNumber(tokens[i])){
+//             if(isNotValidNumber(tokens[i])){
                
                
-                if(tokens[i].equals("+")){
+//                 if(tokens[i].equals("+")){
                    
-                    if(!st.isEmpty()){
-                        int b = st.pop();
-                        int a = st.pop();
-                        int c = b+a;
-                        st.push(c);  
-                    }
+//                     if(!st.isEmpty()){
+//                         int b = st.pop();
+//                         int a = st.pop();
+//                         int c = b+a;
+//                         st.push(c);  
+//                     }
                 
-                }else if(tokens[i].equals("-")){
+//                 }else if(tokens[i].equals("-")){
                     
-                    if(!st.isEmpty()){
-                        int b = st.pop();
-                        int a = st.pop();
-                        int c = a-b;
-                        st.push(c);  
-                    }
+//                     if(!st.isEmpty()){
+//                         int b = st.pop();
+//                         int a = st.pop();
+//                         int c = a-b;
+//                         st.push(c);  
+//                     }
                     
-                }else if(tokens[i].equals("*")){
+//                 }else if(tokens[i].equals("*")){
                     
-                     if(!st.isEmpty()){
-                        int b = st.pop();
-                        int a = st.pop();
-                        int c = b*a;
-                        st.push(c);  
-                    }
+//                      if(!st.isEmpty()){
+//                         int b = st.pop();
+//                         int a = st.pop();
+//                         int c = b*a;
+//                         st.push(c);  
+//                     }
                     
-                }else if(tokens[i].equals("/")){
+//                 }else if(tokens[i].equals("/")){
                     
-                    if(!st.isEmpty()){
-                        int b = st.pop();
-                        int a = st.pop();
+//                     if(!st.isEmpty()){
+//                         int b = st.pop();
+//                         int a = st.pop();
                         
-                        int c = a/b;
-                        st.push(c);  
-                    }
-                }
-            }else{
-                st.push(Integer.parseInt(tokens[i]));
+//                         int c = a/b;
+//                         st.push(c);  
+//                     }
+//                 }
+//             }else{
+//                 st.push(Integer.parseInt(tokens[i]));
+//             }
+//         }
+       
+//         return st.peek();
+//     }
+// }
+
+class Solution {
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                stack.add(stack.pop() + stack.pop());
+            } else if (token.equals("-")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.add(b - a);
+            } else if (token.equals("*")) {
+                stack.add(stack.pop() * stack.pop());
+            } else if (token.equals("/")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.add(b / a);
+            } else {
+                stack.add(Integer.parseInt(token));
             }
         }
-       
-        return st.peek();
+        return stack.pop();
     }
 }
