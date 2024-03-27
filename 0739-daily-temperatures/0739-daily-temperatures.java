f645fbfee297af2d6293099ba8ef79447d3836c8
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2024-03-27 02:36:02 -0400

    Time: 62 ms (65.49%), Space: 62.2 MB (33.65%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,42 +1,42 @@
-class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Pair<Integer,Integer>> st = new Stack<>();
-        int n = temperatures.length;
-        int[] op = new int[n];
-        
-        for(int i=0;i<n;i++){
-                while(!st.isEmpty() &&st.peek().getKey()<temperatures[i]){
-                    int ind = st.pop().getValue();
-                    op[ind] = i-ind;
-            }
-            st.push(new Pair(temperatures[i],i));
-        }
-        
-        while(!st.isEmpty()){
-            int ind =st.peek().getValue();
-            op[ind] = 0;
-            st.pop();
-        }
-        
-        return op;
-    }
-}
-
 // class Solution {
-
 //     public int[] dailyTemperatures(int[] temperatures) {
-//         int[] ans = new int[temperatures.length];
-//         Stack<Integer> stack = new Stack<>();
-//         for (int currDay = 0; currDay < temperatures.length; currDay++) {
-//             while (
-//                 !stack.isEmpty() &&
-//                 temperatures[currDay] > temperatures[stack.peek()]
-//             ) {
-//                 int prevDay = stack.pop();
-//                 ans[prevDay] = currDay - prevDay;
+//         Stack<Pair<Integer,Integer>> st = new Stack<>();
+//         int n = temperatures.length;
+//         int[] op = new int[n];
+        
+//         for(int i=0;i<n;i++){
+//                 while(!st.isEmpty() &&st.peek().getKey()<temperatures[i]){
+//                     int ind = st.pop().getValue();
+//                     op[ind] = i-ind;
 //             }
-//             stack.add(currDay);
+//             st.push(new Pair(temperatures[i],i));
 //         }
-//         return ans;
+        
+//         while(!st.isEmpty()){
+//             int ind =st.peek().getValue();
+//             op[ind] = 0;
+//             st.pop();
+//         }
+        
+//         return op;
 //     }
 // }
+
+class Solution {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] ans = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int currDay = 0; currDay < temperatures.length; currDay++) {
+            while (
+                !stack.isEmpty() &&
+                temperatures[currDay] > temperatures[stack.peek()]
+            ) {
+                int prevDay = stack.pop();
+                ans[prevDay] = currDay - prevDay;
+            }
+            stack.add(currDay);
+        }
+        return ans;
+    }
+}
