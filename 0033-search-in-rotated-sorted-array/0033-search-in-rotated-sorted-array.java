d03dc5cbbd11fdf3ff1de5bcc3266c2aee16ac96
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2024-03-30 20:04:27 -0400

    Time: 0 ms (100.00%), Space: 42.2 MB (27.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,65 +1,65 @@
-// class Solution {
-//     public int search(int[] nums, int target) {
-//         int l=0;
-//         int r = nums.length-1;
-
-//         while(l<r){
-            
-//             int mid = (l+r)/2;
-            
-//             if(nums[mid] == target) return mid;
-            
-            
-//             if(nums[mid]>=nums[l]){
-//                 if(target>nums[mid] || target < nums[l]){
-//                     l = mid+1;
-//                 }else{
-//                     r=mid-1;
-//                 }
-//             }else{
-//              if(target<nums[mid] || target > nums[r]){
-//                    r=mid-1;
-//                 }else{
-//                      l = mid+1;
-//                 }
-//             }
-            
-//         }
-//         return -1;
- 
-//     }
-// }
-
 class Solution {
     public int search(int[] nums, int target) {
-
-        int l = 0;
-        int r = nums.length - 1;
+        int l=0;
+        int r = nums.length-1;
 
         while(l<=r){
-
+            
             int mid = (l+r)/2;
-
-            if(nums[mid] == target){
-                return mid;
-            }
-            //left sorted
-            if(nums[l]<=nums[mid]){
-                if(target > nums[mid] || target < nums[l]){
-                    l = mid + 1;
+            
+            if(nums[mid] == target) return mid;
+            
+            
+            if(nums[mid]>=nums[l]){
+                if(target>nums[mid] || target < nums[l]){
+                    l = mid+1;
                 }else{
-                    r = mid - 1;
+                    r=mid-1;
                 }
-            }else{//right sorted
-                if(target < nums[mid] || target > nums [r]){
-                    r = mid - 1;
+            }else{
+             if(target<nums[mid] || target > nums[r]){
+                   r=mid-1;
                 }else{
-                    l = mid + 1;
+                     l = mid+1;
                 }
             }
-
+            
         }
-
         return -1;
+ 
     }
 }
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+
+//         int l = 0;
+//         int r = nums.length - 1;
+
+//         while(l<=r){
+
+//             int mid = (l+r)/2;
+
+//             if(nums[mid] == target){
+//                 return mid;
+//             }
+//             //left sorted
+//             if(nums[l]<=nums[mid]){
+//                 if(target > nums[mid] || target < nums[l]){
+//                     l = mid + 1;
+//                 }else{
+//                     r = mid - 1;
+//                 }
+//             }else{//right sorted
+//                 if(target < nums[mid] || target > nums [r]){
+//                     r = mid - 1;
+//                 }else{
+//                     l = mid + 1;
+//                 }
+//             }
+
+//         }
+
+//         return -1;
+//     }
+// }
