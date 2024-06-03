017cf2de3b5f79b105a7b06ce37c42a7e3c228f4
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2024-06-03 11:54:57 -0400

    Time: 7 ms (66.78%), Space: 47.6 MB (73.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,26 +1,57 @@
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         Map<String, List<String>> res = new HashMap<>();
+//         for(String s : strs){
+//             int[] count = new int[26];
+//             for(char c : s.toCharArray()){
+//                 count[c-'a']++;
+//             }
+//             StringBuilder sb = new StringBuilder();
+//             for (int i = 0; i < 26; i++) {
+//                 sb.append('#');
+//                 sb.append(count[i]);
+//             }
+            
+//             String key = sb.toString();
+            
+//             if(!res.containsKey(key)){
+//                 res.put(key, new ArrayList<>());
+//             }
+//             res.get(key).add(s);
+            
+            
+//         }
+//         return new ArrayList<>(res.values());
+//     }
+// }
+
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> res = new HashMap<>();
-        for(String s : strs){
-            int[] count = new int[26];
-            for(char c : s.toCharArray()){
-                count[c-'a']++;
+  public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String,List<String>> map=new HashMap<>();
+
+        List<List<String>> result = new ArrayList<>();
+
+
+        for (String word: strs) {
+            char[] charArray = word.toCharArray();
+
+
+            Arrays.sort(charArray);
+
+            String sortedWord = String.valueOf(charArray);
+
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
             }
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 26; i++) {
-                sb.append('#');
-                sb.append(count[i]);
-            }
-            
-            String key = sb.toString();
-            
-            if(!res.containsKey(key)){
-                res.put(key, new ArrayList<>());
-            }
-            res.get(key).add(s);
-            
-            
+
+
+            map.get(sortedWord).add(word);
+
+
         }
-        return new ArrayList<>(res.values());
+        result.addAll(map.values());
+
+        return result;
     }
 }
