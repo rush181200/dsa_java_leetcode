ca4019369a3177009082d1c584a34f369523bc58
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2024-06-26 15:42:05 -0400

    Time: 1113 ms (81.49%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
-SELECT EmployeeUNI.unique_id, Employees.name
-FROM Employees 
-LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id
+# SELECT EmployeeUNI.unique_id, Employees.name
+# FROM Employees 
+# LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id
+
+select eu.unique_id, e.name
+from Employees e
+left join EmployeeUNI eu
+on e.id = eu.id
