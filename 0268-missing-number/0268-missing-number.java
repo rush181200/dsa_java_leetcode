f1af9dae56f468f0dfa35196cc8a768d16e675f7
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2024-06-28 16:58:26 -0400

    Time: 2 ms (29.78%), Space: 44.8 MB (85.46%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -17,16 +17,29 @@
 //     }
 // }
 
-public class Solution {
+
+// XOR
+// public class Solution {
+//     public int missingNumber(int[] nums) {
+//         int n = nums.length;
+//         int ans = 0;
+//         for (int i = 1; i <= n; i++) {
+//             ans = ans ^ i;
+//         }
+//         for (int i = 0; i < nums.length; i++) {
+//             ans = ans ^ nums[i];
+//         }
+//         return ans;
+//     }
+// }
+
+// All Sum Approach
+
+class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            ans = ans ^ i;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            ans = ans ^ nums[i];
-        }
-        return ans;
+        int Tsum = (n * (n + 1)) / 2;
+        int actualSum = Arrays.stream(nums).sum();
+        return Tsum - actualSum;
     }
 }
