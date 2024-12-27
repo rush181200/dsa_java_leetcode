6fe476772c65d1470ec14705a98230f67800d70a
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2024-12-27 17:01:04 -0500

    Time: 46 ms (5.34%), Space: 44.9 MB (5.94%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int getLucky(String s, int k) {
+       String combine="";
+
+        for(char c : s.toCharArray()){
+            combine = combine + (c-'a'+1);
+        }
+    
+       int ans = 0;
+       System.out.println(combine);
+       while(k>0){
+        System.out.println("-------==----------------==================");
+         ans=0;
+        for(int i=0;i<combine.length();i++){
+            int digit = combine.charAt(i)-'0';
+            System.out.println(digit);
+            ans+=digit;
+        }
+        
+        combine = String.valueOf(ans);
+       
+        k--;
+       }
+         
+   
+       return ans;
+    }
+}
