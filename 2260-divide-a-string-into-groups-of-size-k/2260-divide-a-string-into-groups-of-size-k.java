9ae017e6dbee0f883cf285f4bdd25385e4b58008
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-01-01 18:41:51 -0500

    Time: 5 ms (36.29%), Space: 43.5 MB (5.79%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.java
@@ -1,17 +1,14 @@
 class Solution {
     public String[] divideString(String str, int size, char fill) {
-        if (str == null || size <= 0) {
-            throw new IllegalArgumentException("Invalid input or chunk size.");
-        }
+        // if (str == null || size <= 0) {
+        //     throw new IllegalArgumentException("Invalid input or chunk size.");
+        // }
 
         List<String> result = new ArrayList<>();
-
-        // Add 'x' padding to ensure the string length is a multiple of the chunk size
         while (str.length() % size != 0) {
             str += fill;
         }
 
-        // Split into chunks of the specified size
         for (int i = 0; i < str.length(); i += size) {
             result.add(str.substring(i, i + size));
         }
