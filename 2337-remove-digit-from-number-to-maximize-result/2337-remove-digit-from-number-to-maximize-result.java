29dacb7cd024eac353ab49d235702275e4ae4a10
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-01-02 17:36:02 -0500

    Time: 2 ms (57.22%), Space: 42.2 MB (36.6%) - LeetHub

--- 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.java
+++ 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.java
@@ -5,14 +5,7 @@ class Solution {
        
         for(int i=0;i<n;i++){
             if(s.charAt(i)==digit){
-                // if(i==0){
-                //    curr = s.substring(i+1);
-                // }else if(i==n-1){
-                //     curr = s.substring(0,n-1);
-                // }else{
-                //     curr = s.substring(0,i-1)+s.substring(i+1,n);
-                // }
-                // maxi = Math.max(maxi,Integer.parseInt(curr));
+                
                 String curr = s.substring(0, i) + s.substring(i + 1);
                 // Compare lexicographically to get the maximum
                 if (result.isEmpty() || curr.compareTo(result) > 0) {
