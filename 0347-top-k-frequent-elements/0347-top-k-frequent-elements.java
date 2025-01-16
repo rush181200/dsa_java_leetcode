dc1465b5c057c56c554597a86ff6290e21c9f59e
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-01-16 14:00:20 -0500

    Time: 12 ms (81.83%), Space: 48.7 MB (49.56%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,82 +1,33 @@
-// Bucket Sort
+import java.util.*;
+
 class Solution {
-     /**
-     * Time Complexity: O(n)
-     * Space Complexity: O(n)
-     */
-     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> count = new HashMap<>();
-        List<Integer> bucket[] = new ArrayList[nums.length + 1];                
+    public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
+        // Frequency map
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Priority queue (min-heap) based on frequency
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = 
+            new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
         
-        for (int num : nums)
-            count.merge(num, 1, Integer::sum);
-        
-        for (int key : count.keySet()){
-            int freq = count.get(key);
-            if (bucket[freq] == null)
-                bucket[freq] = new ArrayList<>();
-            bucket[freq].add(key);
+        // Add entries to the heap, keeping only the k most frequent elements
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            minHeap.add(entry);
+            if (minHeap.size() > k) {
+                minHeap.poll();  // Remove the least frequent element
+            }
         }
         
+        // Extract the top k frequent elements
+        int[] ans = new int[k];
         int index = 0;
-        int[] res = new int[k];
-        for (int i = nums.length; i >= 0; i--)
-            if (bucket[i] != null)
-                for (int val : bucket[i]){
-                    res[index++] = val;
-                    if(index == k)
-                        return res;
-                }
-        return res;
+        while (!minHeap.isEmpty()) {
+            ans[index++] = minHeap.poll().getKey();  // Get the key (number)
+        }
+
+        return ans;
     }
 }
-// class Solution {
-//     public int[] topKFrequent(int[] nums, int k) {
-        
-        
-//         Map<Integer,Integer> map = new HashMap<>();
-//        List<Integer> arr = new ArrayList<>();
-//         for(int n:nums){
-//             if(map.containsKey(n)){
-//                 map.put(n,map.get(n)+1);
-//             }else{
-//                 map.put(n,1); 
-//             }
-           
-//         }
-//          Map<Integer, Integer> sortedMap = map.entrySet()
-//             .stream()
-//             .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
-//             .collect(Collectors.toMap(
-//                 Map.Entry::getKey,
-//                 Map.Entry::getValue,
-//                 (e1, e2) -> e1,
-//                 LinkedHashMap::new
-//             ));
-
-//         // Print the sorted map
-//         System.out.println("Sorted map (descending by values): " + sortedMap);
-//         int count = k;
-//         for (Map.Entry<Integer, Integer> entry : sortedMap.entrySet()){
-//             System.out.println("map key"+entry.getKey());
-//              System.out.println("map value"+entry.getValue());
-            
-          
-//                arr.add(entry.getKey());
-             
-//         }
-//         System.out.println("array"+arr);
-        
-//         int[] ans = new int[k];
-//         for(int i=0;i<arr.size();i++){
-//             System.out.println("arr"+arr.get(i));
-//             if(count>0){
-//                 ans[i] = arr.get(i);
-//             }
-//             count--;
-            
-//         }
-        
-//         return ans;
-//     }
-// }
