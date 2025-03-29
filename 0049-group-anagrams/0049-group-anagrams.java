cd3f2b242793b7725c05e3cfe0c5a76d94148d52
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-03-29 16:43:51 -0400

    Time: 7 ms (66.68%), Space: 47.6 MB (95.35%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,57 +1,21 @@
-// class Solution {
-//     public List<List<String>> groupAnagrams(String[] strs) {
-//         Map<String, List<String>> res = new HashMap<>();
-//         for(String s : strs){
-//             int[] count = new int[26];
-//             for(char c : s.toCharArray()){
-//                 count[c-'a']++;
-//             }
-//             StringBuilder sb = new StringBuilder();
-//             for (int i = 0; i < 26; i++) {
-//                 sb.append('#');
-//                 sb.append(count[i]);
-//             }
-            
-//             String key = sb.toString();
-            
-//             if(!res.containsKey(key)){
-//                 res.put(key, new ArrayList<>());
-//             }
-//             res.get(key).add(s);
-            
-            
-//         }
-//         return new ArrayList<>(res.values());
-//     }
-// }
-
 class Solution {
-  public List<List<String>> groupAnagrams(String[] strs) {
-
-        HashMap<String,List<String>> map=new HashMap<>();
-
-        List<List<String>> result = new ArrayList<>();
-
-
-        for (String word: strs) {
-            char[] charArray = word.toCharArray();
-
-
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans = new ArrayList<>();
+        Map<String,List<String>> map = new HashMap<>();
+        for(String str : strs){
+            char[] charArray = str.toCharArray();
             Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
 
-            String sortedWord = String.valueOf(charArray);
-
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>()); 
             }
-
-
-            map.get(sortedWord).add(word);
-
-
+            map.get(sortedStr).add(str); // Now adding the word to the list
         }
-        result.addAll(map.values());
 
-        return result;
+        for (List<String> value : map.values()) {
+            ans.add(value);
+        }
+        return ans;
     }
 }
