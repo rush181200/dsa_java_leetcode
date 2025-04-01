e7fd7527fe4071f2a200676be1b57a02891e4a64
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-01 11:39:01 -0400

    Time: 6 ms (75.5%), Space: 44.8 MB (53.73%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,82 +1,31 @@
 class Solution {
-   
     public int evalRPN(String[] tokens) {
-        
         Stack<Integer> st = new Stack<>();
-       
-        
-        for(int i=0;i<tokens.length;i++){
+        for(String s:tokens){
             
-           
-               
-               
-                if(tokens[i].equals("+")){
-                   
-                    if(!st.isEmpty()){
-                        int b = st.pop();
-                        int a = st.pop();
-                        int c = b+a;
-                        st.push(c);  
-                    }
-                
-                }else if(tokens[i].equals("-")){
-                    
-                    if(!st.isEmpty()){
-                        int b = st.pop();
-                        int a = st.pop();
-                        int c = a-b;
-                        st.push(c);  
-                    }
-                    
-                }else if(tokens[i].equals("*")){
-                    
-                     if(!st.isEmpty()){
-                        int b = st.pop();
-                        int a = st.pop();
-                        int c = b*a;
-                        st.push(c);  
-                    }
-                    
-                }else if(tokens[i].equals("/")){
-                    
-                    if(!st.isEmpty()){
-                        int b = st.pop();
-                        int a = st.pop();
-                        
-                        int c = a/b;
-                        st.push(c);  
-                    }
-                }
-            else{
-                st.push(Integer.parseInt(tokens[i]));
+            if(s.equals("+") && !st.isEmpty()){
+                int a = st.pop();
+                int b = st.pop();
+                int c = a+b;
+                st.push(c);
+            }else if(s.equals("-") && !st.isEmpty()){
+                int a = st.pop();
+                int b = st.pop();
+                int c = b-a;
+                st.push(c);
+            }else if(s.equals("*") && !st.isEmpty()){
+                int a = st.pop();
+                int b = st.pop();
+                int c = a*b;
+                st.push(c);
+            }else if(s.equals("/") && !st.isEmpty()){
+                int a = st.pop();
+                int b = st.pop();
+                int c = b/a;
+                st.push(c);
+            }else{
+                 st.push(Integer.parseInt(s));
             }
-        }
-       
-        return st.peek();
+        }return st.peek();
     }
 }
-
-// class Solution {
-
-//     public int evalRPN(String[] tokens) {
-//         Stack<Integer> stack = new Stack<>();
-//         for (String token : tokens) {
-//             if (token.equals("+")) {
-//                 stack.add(stack.pop() + stack.pop());
-//             } else if (token.equals("-")) {
-//                 int a = stack.pop();
-//                 int b = stack.pop();
-//                 stack.add(b - a);
-//             } else if (token.equals("*")) {
-//                 stack.add(stack.pop() * stack.pop());
-//             } else if (token.equals("/")) {
-//                 int a = stack.pop();
-//                 int b = stack.pop();
-//                 stack.add(b / a);
-//             } else {
-//                 stack.add(Integer.parseInt(token));
-//             }
-//         }
-//         return stack.pop();
-//     }
-// }
