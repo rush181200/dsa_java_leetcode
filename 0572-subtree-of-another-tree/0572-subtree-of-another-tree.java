2c7b5c3f31a52ae3eedaa4c4d6045434bc439194
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-01 19:13:13 -0400

    Time: 2 ms (97.44%), Space: 44.3 MB (81.5%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -14,19 +14,30 @@
  * }
  */
 class Solution {
-       public boolean isSubtree(TreeNode p, TreeNode q) {
-        if (p == null) return false;
-
-        if (isSameTree(p, q)) return true;
-
-        return isSubtree(p.left, q) || isSubtree(p.right, q);
+    public boolean isSubtree(TreeNode p, TreeNode q) {
+         if (p == null) return false;
+       if (isSameTree(p, q)) return true;
+        return isSubtree(p.left,q) ||  isSubtree(p.right,q);
     }
-
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        if (p.val != q.val) return false;
+         if (p == null && q == null) return true;
+         if (p == null || q == null) return false;
+         if (p.val != q.val) return false;
 
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
+
+    //     public boolean isSubtree(TreeNode p, TreeNode q) {
+    //     if (p == null) return false;
+    //     if (isSameTree(p, q)) return true;
+    //     return isSubtree(p.left, q) || isSubtree(p.right, q);
+    // }
+
+    // public boolean isSameTree(TreeNode p, TreeNode q) {
+    //     if (p == null && q == null) return true;
+    //     if (p == null || q == null) return false;
+    //     if (p.val != q.val) return false;
+
+    //     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    // }
 }
