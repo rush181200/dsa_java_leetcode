022ac25362f3daef3f1819238dc3c23c70656cd8
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-02 16:06:44 -0400

    Time: 3 ms (6.74%), Space: 44.2 MB (93.61%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -16,9 +16,11 @@
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        dfs(root,k,maxHeap);
         // fails when k=2 and one is on right and other on left
-        maxHeap.offer(root.val);
-        dfs(root.left,k,maxHeap);
+        // maxHeap.offer(root.val);
+        // dfs(root.left,k,maxHeap);
+        // 
         if(maxHeap.size()<k) dfs(root.right,k,maxHeap);
         return maxHeap.peek();
     }
