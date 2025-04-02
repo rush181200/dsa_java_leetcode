870f3712de86c881223ba61889ab70a93d2cb4fc
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-01 21:17:05 -0400

    Time: 60 ms (81.72%), Space: 56.2 MB (97.09%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,50 +1,17 @@
-// class Solution {
-//     public int[] dailyTemperatures(int[] temperatures) {
-//         Stack<Pair<Integer,Integer>> st = new Stack<>();
-//         int n = temperatures.length;
-//         int[] op = new int[n];
-//             Problem when similar comes --> [89,62,70,58,47,47,46,76,100,70]
-//         for(int i=0;i<n;i++){
-//             if(st.isEmpty()){
-//                 st.push(new Pair(temperatures[i],i));
-//             }else if(st.peek().getKey()<temperatures[i]){
-//                 while(!st.isEmpty()&&st.peek().getKey()<temperatures[i]){
-//                     int ind = st.pop().getValue();
-                    
-//                     op[ind] = i-ind;  
-//                 }
-//                 st.push(new Pair(temperatures[i],i));
-                
-//             }else if(st.peek().getKey()>temperatures[i]){
-//                 st.push(new Pair(temperatures[i],i));
-//             }
-//         }
-        
-//         while(!st.isEmpty()){
-//             int ind =st.peek().getValue();
-//             op[ind] = 0;
-//             st.pop();
-//         }
-        
-//         return op;
-//     }
-// }
-
 class Solution {
-
     public int[] dailyTemperatures(int[] temperatures) {
-        int[] ans = new int[temperatures.length];
-        Stack<Integer> stack = new Stack<>();
-        for (int currDay = 0; currDay < temperatures.length; currDay++) {
-            while (
-                !stack.isEmpty() &&
-                temperatures[currDay] > temperatures[stack.peek()]
-            ) {
-                int prevDay = stack.pop();
-                ans[prevDay] = currDay - prevDay;
-            }
-            stack.add(currDay);
+        Stack<int[]> st = new Stack<>();
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty()&& temperatures[i]>st.peek()[0]){
+                    int[] curr = st.pop();
+                    ans[curr[1]] = i-curr[1];
+                    
+                }
+                st.push(new int[]{temperatures[i],i});
         }
+        
         return ans;
     }
 }
