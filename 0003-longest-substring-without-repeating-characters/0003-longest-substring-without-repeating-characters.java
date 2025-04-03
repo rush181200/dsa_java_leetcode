6126ae3fa9709cbf4582a5a852c436f136c95f8b
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-03 00:55:07 -0400

    Time: 6 ms (70.58%), Space: 44.9 MB (42.76%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,47 +1,17 @@
-class Solution {
+public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> hs = new HashSet<>();
-        int left=0;
-        int right=0;
-        int n = s.length();
-        int maxlen =0;
-        
-        while(right<n){
-            while(hs.contains(s.charAt(right))&&left<right){
-                hs.remove(s.charAt(left));
-                left+=1;
+        Set<Character> charSet = new HashSet<>();
+        int l = 0, res = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            while (charSet.contains(s.charAt(r))) {
+                charSet.remove(s.charAt(l));
+                l++;
             }
-            hs.add(s.charAt(right));
-            maxlen = Math.max(maxlen,right-left+1);
-            right++;
+            charSet.add(s.charAt(r));
+            res = Math.max(res, r - l + 1);
         }
-        return maxlen;
-            
-//         int llen = 0;
-//         int left=0;
-//         int right=0;
-//         int n = s.length();
-//         ArrayList<Character> map = new ArrayList<>();
-        
-        
-//         while(right<n){
-//             char ch = s.charAt(right);
-            
-        
-//            if(map.contains(s.charAt(right) )&& left<=right){
-//                llen += Math.max(llen,map.size());
-               
-//                     map.remove(s.charAt(left));
-//                     left++;
-//            }  
-//                if(right<n){
-//                      map.add(s.charAt(right));
-//                      right++;
-//                }
- 
-            
-            
-//         }
-//         return llen;
+
+        return res;
     }
 }
