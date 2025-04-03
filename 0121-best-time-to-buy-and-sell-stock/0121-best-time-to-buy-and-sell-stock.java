43d7c93a786da08ff6515bc2fe168d79144f6cab
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-03 17:06:33 -0400

    Time: 2 ms (74.7%), Space: 61.7 MB (63.53%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,32 +1,17 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        // Brute Force
-        // int maxPro=0;
-        // int n = prices.length;
-        // for(int i=0;i<=n-2;i++){
-        //     for(int j=i+1;j<=n-1;j++){
-        //         if(prices[i]<prices[j]){
-        //             int maxP = prices[j]-prices[i];
-        //             maxPro = Math.max(maxPro,maxP);
-        //         }
-        //     }
-        // }
-        // return maxPro;
-        int maxPro=0;
-        int n = prices.length;
-        int l = 0;
-        int r = l+1;
-        
-        while(r<n){
-            if(prices[r]>prices[l]){
-                int profit = prices[r]-prices[l];
-                 maxPro = Math.max(maxPro,profit);
-                r++;
-            }else{
+    public int maxProfit(int[] arr) {
+        int maxP = 0;
+        int n = arr.length-1;
+        int l=0;
+        int r=1;
+        while(r<=n){
+            if(arr[l]>=arr[r]){
                 l=r;
-                r++;
+            }else if(arr[r]>arr[l]){
+                maxP = Math.max(maxP,arr[r]-arr[l]);
             }
+            r++;
         }
-        return maxPro;
+        return maxP;
     }
 }
