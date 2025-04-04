61af10ba781d1665e855790fa0358a325156c459
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-03 20:05:21 -0400

    Time: 0 ms (100%), Space: 41.8 MB (86.72%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,37 +1,16 @@
-// class Solution {
-//     public int findMin(int[] nums) {
-//         int n = nums.length;
-//         int r = n-1;
-//         int l=0;
-//         while(l<=r){
-//             int mid = l+(r-l)/2;
-//             if(nums[mid]>nums[r]){
-//                 l = mid+1;
-//             }else {
-//                 r= mid;
-//            }
-//         }
-//         return nums[l];
-//     }
-// }
-
 class Solution {
     public int findMin(int[] nums) {
-        int l = 0;
-        int r = nums.length - 1;
-
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-
-            // If mid element is greater than right, min must be on the right
-            if (nums[mid] > nums[r]) {
-                l = mid + 1;
-            } else {
-                // mid might be the smallest, so include it
-                r = mid;
-            }
+        int n = nums.length;
+        int r = n-1;
+        int l=0;
+        while(l<r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]>nums[r]){
+                l = mid+1;
+            }else {
+                r= mid;
+           }
         }
-
-        return nums[l]; // or nums[r] — both are same when loop ends
+        return nums[l];
     }
 }
