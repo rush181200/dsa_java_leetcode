9d51975de5cb8051095ca9fe18aaa242ff469c84
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-06 18:00:10 -0400

    Time: 53 ms (21.47%), Space: 115.3 MB (42.99%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,43 +1,45 @@
+
+
 class LRUCache {
     Node head = new Node(0, 0), tail = new Node(0, 0);
     Map < Integer, Node > map = new HashMap();
     int capacity;
-
-    public LRUCache(int _capacity) {
-        capacity = _capacity;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
         head.next = tail;
         tail.prev = head;
     }
-
+    
     public int get(int key) {
-        if (map.containsKey(key)) {
-            Node node = map.get(key);
-            remove(node);
-            insert(node);
-            return node.value;
-        } else {
-            return -1;
+        
+        if(map.containsKey(key)){
+           Node node = map.get(key);
+           removeNode(node);
+           addNode(node);
+           return node.value;
         }
+        return -1;
     }
-
+    
     public void put(int key, int value) {
         if (map.containsKey(key)) {
-            remove(map.get(key));
+            removeNode(map.get(key));
         }
         if (map.size() == capacity) {
-            remove(tail.prev);
+            removeNode(tail.prev);
         }
-        insert(new Node(key, value));
+        addNode(new Node(key, value));
+
     }
 
-    private void remove(Node node) {
+    public void removeNode(Node node){
         map.remove(node.key);
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
 
-    private void insert(Node node) {
-        map.put(node.key, node);
+    public void addNode(Node node){
+        map.put(node.key,node);
         node.next = head.next;
         node.next.prev = node;
         head.next = node;
@@ -53,6 +55,7 @@ class LRUCache {
         }
     }
 }
+
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
