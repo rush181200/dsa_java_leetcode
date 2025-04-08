ef04d5fc433ea13fbc5c7ec9677db3f37377cd80
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-07 20:28:58 -0400

    Time: 0 ms (100%), Space: 41.6 MB (87.03%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,9 +1,19 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0) return 0;
-        int[] dp = new int[s.length()];
-        Arrays.fill(dp, -1);
-        return decodeWays(0, s,dp);
+        int n=s.length();
+        int[] dp=new int[n+1];
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--)
+            if(s.charAt(i)!='0') {
+                dp[i]=dp[i+1];
+                if(i<n-1&&(s.charAt(i)=='1'||s.charAt(i)=='2'&&s.charAt(i+1)<'7')) 
+					dp[i]+=dp[i+2];
+            }
+        return dp[0];   
+
+        // MemoIzation
+        // Arrays.fill(dp, -1);
+        // return decodeWays(0, s,dp);
     }
 
     public int decodeWays(int index, String s,int[] dp) {
