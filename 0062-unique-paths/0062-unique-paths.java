1f910f436fe2f275387ae2ef9bfda231ba117374
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-19 01:02:02 -0400

    Time: 1 ms (8.58%), Space: 40.5 MB (47.7%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,7 +4,23 @@ class Solution {
         for(int i = 0; i < m; i++) {
             Arrays.fill(dp[i], -1);
         }   
-        return letsReach(m-1,n-1,dp);
+        for(int i = 0; i < m; i++) {
+           for(int j =0;j<n;j++){
+            if(i==0&&j==0) dp[0][0] =1;
+            else{
+            int up =0;
+            int left =0;
+            if(i>0)
+            up = dp[i-1][j];
+            if(j>0)
+            left = dp[i][j-1];
+            dp[i][j] = up+left;
+            }
+            
+           }
+        } 
+        return dp[m-1][n-1];
+        // return letsReach(m-1,n-1,dp);
     }
 
     // recurrsion
