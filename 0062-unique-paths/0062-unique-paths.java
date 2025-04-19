7e12ea1c7d06f3be272e2c1b9afc76f00010fc42
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-19 01:25:17 -0400

    Time: 0 ms (100%), Space: 39.9 MB (98.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,26 +1,27 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-          
-        for(int i = 0; i < m; i++) {
-            int[] temp = new int[n];
-           for(int j =0;j<n;j++){
-            if(i==0&&j==0) temp[j] =1;
-            else{
-                int up =0;
-                int left =0;
-                if(i>0)
-                up = dp[j];
-                if(j>0)
-                left = temp[j-1];
-                temp[j] = up+left;
-            }
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++) 
+        Arrays.fill(dp[i], -1);
+          // space optimization
+        // for(int i = 0; i < m; i++) {
+        //     int[] temp = new int[n];
+        //    for(int j =0;j<n;j++){
+        //     if(i==0&&j==0) temp[j] =1;
+        //     else{
+        //         int up =0;
+        //         int left =0;
+        //         if(i>0)
+        //         up = dp[j];
+        //         if(j>0)
+        //         left = temp[j-1];
+        //         temp[j] = up+left;
+        //     }
             
-           }
-           dp = temp;
-        } 
-        return dp[n-1];
+        //    }
+        //    dp = temp;
+        // } 
+        // return dp[n-1];
         // Tabulation 
         // for(int i = 0; i < m; i++) {
         //    for(int j =0;j<n;j++){
@@ -39,7 +40,7 @@ class Solution {
         // } 
         // return dp[m-1][n-1];
         // Memoization
-        // return letsReach(m-1,n-1,dp);
+        return letsReach(m-1,n-1,dp);
     }
 
     // recurrsion
