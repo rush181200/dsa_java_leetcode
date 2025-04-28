fa47017279373a4a316e70e878f06a54fc6d57f2
RUSHABH MEHTA <47397248+rush181200@users.noreply.github.com>
2025-04-28 16:30:19 -0400

    Time: 50 ms (46.03%), Space: 57.5 MB (32.96%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let counter = init;
+    return {
+        increment: function(){
+            return ++counter;
+        },
+        decrement: function(){
+            return --counter;
+        },
+        reset: function(){
+            counter=init
+            return counter;
+        },
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
